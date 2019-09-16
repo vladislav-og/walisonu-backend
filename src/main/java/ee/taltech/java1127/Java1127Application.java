62ad5e50e2596184934f420cfe3b26812eeb4210
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-09-16 09:38:15 +0300

    Initial commit

---
+++ src/main/java/ee/taltech/java1127/Java1127Application.java
@@ -0,0 +1,13 @@
+package ee.taltech.java1127;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java1127Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java1127Application.class, args);
+	}
+
+}
---
+++ src/test/java/ee/taltech/java1127/Java1127ApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.taltech.java1127;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java1127ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
