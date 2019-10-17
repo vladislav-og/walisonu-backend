ffab6c012399e0a58ae26c5b37d9d23aec0992ae
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-10-17 13:13:08 +0300

    Add get method

--- src/main/java/ee/taltech/java1127/controller/WordController.java
+++ src/main/java/ee/taltech/java1127/controller/WordController.java
@@ -1,9 +1,12 @@
 package ee.taltech.java1127.controller;
 
+import ee.taltech.java1127.dto.WordDto;
 import ee.taltech.java1127.model.Word;
 import ee.taltech.java1127.service.WordService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,11 @@ public class WordController {
     @GetMapping
     public List<Word> getAllWords() {
         return wordService.getAllWords();
+    }
+
+    @PostMapping
+    public WordDto saveWord(@RequestBody WordDto wordDto) {
+        return wordService.createNewWord(wordDto);
     }
 
 
