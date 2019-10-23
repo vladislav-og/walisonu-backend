ff6c8cbfc43248f5fad798b9fadc05ec5ae1293d
Egle <egpalk@ttu.ee>
2019-10-23 23:38:10 +0300

    Add new configuration file and build.radle

--- src/main/java/ee/taltech/java1127/model/User.java
+++ src/main/java/ee/taltech/java1127/model/User.java
@@ -10,34 +10,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.Email;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class Users {
 
+
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    //@SequenceGenerator(name = "user_seq", sequenceName = "users_sequence", allocationSize = 1)
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
-    @SequenceGenerator(name = "user_seq", sequenceName = "users_sequence", allocationSize = 1)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long user_id;
-    @Email
     @Column(unique = true)
     private String email;
 
 
-    public User(@Email String email) {
+    public Users(String email) {
         this.email = email;
     }
 
-    public User(Long id, String email) {
+    public Users(Long id, String email) {
         this.user_id = id;
         this.email = email;
     }
 
-    public User(UserDto userDao) {
+    public Users(UserDto userDao) {
         this.user_id = userDao.getId();
         this.email = userDao.getEmail();
     }
