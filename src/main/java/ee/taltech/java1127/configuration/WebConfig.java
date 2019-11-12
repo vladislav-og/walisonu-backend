151ed1b858c22fc2d5e38533cfb9e38504895576
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-11-12 21:53:13 +0200

    Add all origins

--- src/main/java/ee/taltech/java1127/configuration/WebConfig.java
+++ src/main/java/ee/taltech/java1127/configuration/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://walisonu.ga").allowedMethods("GET", "POST","PUT", "DELETE");
+                .allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 }
